5a84ef41ef7af92e719fc551555bdb44728f2334
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-05-07 17:42:00 +0530

    User rest end points created

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
-use actix_web::{route, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-#[route("/hey", method = "GET")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("Server  is running!")
 }
 
-#[route("/hey", method = "POST")]
-async fn echo(body: String) -> impl Responder {
-    HttpResponse::Ok().body(body)
+async fn get_users() -> impl Responder {
+    HttpResponse::Ok().json("[{id:1, name:\"Qng\"}, {id:2, name:\"Ada\"}]")
+}
+async fn get_user_details() -> impl Responder {
+    HttpResponse::Ok().json("{id:1, name:\"Qng\"}")
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn create_user() -> impl Responder {
+    HttpResponse::Ok().json("{ msg:\"User Created Successfully!\"}")
 }
+
+async fn update_user() -> impl Responder {
+    HttpResponse::Ok().json("{ msg:\"User Updated Successfully!\"}")
+}
+
+async fn delete_user() -> impl Responder {
+    HttpResponse::Ok().json("{ msg:\"User Deleted Successfully!\"}")
+}
+
+const PORT: u16 = 8080;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("http://localhost:{}", PORT);
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .route("/", web::get().to(health_check))
+            .route("/users", web::get().to(get_users))
+            .route("/users/{id}", web::get().to(get_user_details))
+            .route("/users", web::post().to(create_user))
+            .route("/users/{id}", web::patch().to(update_user))
+            .route("/users/{id}", web::delete().to(delete_user))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", PORT))?
     .run()
     .await
 }
